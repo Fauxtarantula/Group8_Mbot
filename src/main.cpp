123d60ac5d101ac5b5b0264248ad2208e25022f8
Muhammad Rakin <68525714+Fauxtarantula@users.noreply.github.com>
2025-03-18 16:59:02 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,21 @@
+#include "MeMCore.h"
+//#include "Motor_control.h"
+MeDCMotor motor1(M1);
+MeDCMotor motor2(M2);
 
+void setup(){
+  
+  
+}
+
+void loop(){
+  motor1.run(255);
+  motor2.run(255);
+  delay(1000);
+  motor1.stop();
+  motor2.stop();
+  delay(100);
+  motor1.run(-255);
+  motor2.run(-255);
+  delay(1000);
+}
